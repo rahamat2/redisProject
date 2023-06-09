b35d4e9dde01f4b88443e890b85fa9615a432843
Rahamat-E-Elahi <rahamatelahi4@gmail.com>
2023-06-09 07:42:34 -0700

    Post to db success

--- apiServer/server.js
+++ apiServer/server.js
@@ -7,9 +7,13 @@ if(process.env.NODE_ENV != 'production'){
 //Import dependencies
 const express = require('express');
 const connectToDb = require('./config/connectToDb');
+const Note = require('./models/note');
 
 //Create our express app
 const app = express()
+
+//Configure express app
+app.use(express.json());
 
 //Connect to database
 connectToDb();
@@ -17,6 +21,21 @@ connectToDb();
 //Routing
 app.get('/', (req,res)=>{
     res.json({ hello:"world" });
+});
+
+app.post('/notes', async (req,res)=>{
+    //Get the data from request body
+    const title = req.body.title;
+    const body = req.body.body;
+
+    //Create a note with it
+    const note = await Note.create({
+        title: title,
+        body: body
+    });
+
+    //Respond with the new note
+    res.json({ note: note });
 });
 
 //Start our server
