7a10aa990b14d16302ec036396af91ca1857115c
Rahamat-E-Elahi <rahamatelahi4@gmail.com>
2023-06-09 08:18:50 -0700

    CRUD works fine

--- apiServer/server.js
+++ apiServer/server.js
@@ -23,6 +23,45 @@ app.get('/', (req,res)=>{
     res.json({ hello:"world" });
 });
 
+app.get('/notes', async (req,res)=>{
+    //Find the notes
+    const notes = await Note.find();
+    //Respond with them
+    res.json({notes: notes});
+});
+
+app.get('/notes/:id', async (req,res)=>{
+    //Get the id from the URL
+    const noteId = req.params.id;
+
+    //Find the note using that id
+    const note = await Note.findById(noteId);
+
+    //Respond with the note
+    res.json({ note: note });
+});
+
+app.put('/notes/:id', async (req,res)=>{
+    //Get the id from the URL
+    const noteId = req.params.id;
+
+    //Get the data from request body
+    const title = req.body.title;
+    const body = req.body.body;
+
+    //Find the note and update using that id
+    const noteOld = await Note.findByIdAndUpdate(noteId, {
+        title: title,
+        body: body,
+    });
+
+    //Find the updated note
+    const noteUpdated = await Note.findById(noteId);
+
+    //Respond with the note
+    res.json({ noteOld: noteOld, noteUpdated: noteUpdated });
+});
+
 app.post('/notes', async (req,res)=>{
     //Get the data from request body
     const title = req.body.title;
@@ -36,6 +75,17 @@ app.post('/notes', async (req,res)=>{
 
     //Respond with the new note
     res.json({ note: note });
+});
+
+app.delete('/notes/:id', async (req,res)=>{
+    //Get the id from the URL
+    const noteId = req.params.id;
+
+    //Find the note using that id
+    await Note.deleteOne({ _id: noteId});
+
+    //Respond with the note
+    res.json("Delete Success");
 });
 
 //Start our server
